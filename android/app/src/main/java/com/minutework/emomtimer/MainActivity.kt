2f3e98f25c05008f5c4c00db62488a14cada36b0
Chris Witalewski <witalewski@gmail.com>
2026-09-14 20:19:19 +0200

    Fix Android layout overlapping system bars

--- android/app/src/main/java/com/minutework/emomtimer/MainActivity.kt
+++ android/app/src/main/java/com/minutework/emomtimer/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.minutework.emomtimer
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    // Keep the React layout inside the system bars and camera cutout while the
+    // window background continues to draw edge-to-edge.
+    val content = findViewById<View>(android.R.id.content)
+    ViewCompat.setOnApplyWindowInsetsListener(content) { view, windowInsets ->
+      val insets = windowInsets.getInsets(
+          WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout()
+      )
+      view.setPadding(insets.left, insets.top, insets.right, insets.bottom)
+      WindowInsetsCompat.CONSUMED
+    }
+    ViewCompat.requestApplyInsets(content)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
